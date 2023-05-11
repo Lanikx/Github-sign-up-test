ab41a09bc2954e28a72b86460b901497045b4bfa
Mikita-Lapin <64269242+Lanikx@users.noreply.github.com>
2023-05-11 14:27:19 +0300

    Sign up test

---
+++ test1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace test1
+public class Class1
+{
+	public Class1()
+	{
+
+		
+	}
+    public static void Main()
+    { }
+
+    [TestCase]
+    void Test1()
+    {
+        WebDriver driver = WebDriver();
+        driver.Get("https://github.com/");
+        driver.FindElement(By.Xpath,)
+
+
+        }
+}
---
+++ test1/test1/testing.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    [TestFixture]   
+    public class testing
+    {
+        [TestCase]
+        public void Testgit()
+        {
+            WebDriver driver = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            driver.Navigate().GoToUrl("https://github.com");
+            driver.Manage().Window.Maximize();
+            wait.Until(d => driver.FindElement(By.XPath("//header//a[contains(text(), 'Sign up')]")));
+            driver.FindElement(By.XPath("//header//a[contains(text(), 'Sign up')]")).Click();
+
+            wait.Until(d => driver.FindElement(By.XPath("//input[@id='email']")).Displayed);
+            driver.FindElement(By.XPath("//input[@id='email']")).SendKeys("12adsdqw13@gmail.com");
+            wait.Until(d => driver.FindElement(By.XPath("//div[@id='email-container']/div/button")).Enabled);
+            driver.FindElement(By.XPath("//div[@id='email-container']/div/button")).Click();
+
+            wait.Until(d => driver.FindElement(By.XPath("//input[@id='password']")).Displayed);
+            driver.FindElement(By.XPath("//input[@id='password']")).SendKeys("12adsdqw13@gmail.com");
+            wait.Until(d => driver.FindElement(By.XPath("//div[@id='password-container']/div/button")).Enabled);
+            driver.FindElement(By.XPath("//div[@id='password-container']/div/button")).Click();
+
+            wait.Until(d => driver.FindElement(By.XPath("//input[@id='login']")).Displayed);
+            driver.FindElement(By.XPath("//input[@id='login']")).SendKeys("adsdqw13gdasd12mailcom");
+            wait.Until(d => driver.FindElement(By.XPath("//div[@id='username-container']/div/button")).Enabled);
+            driver.FindElement(By.XPath("//div[@id='username-container']/div/button")).Click();
+
+            wait.Until(d => driver.FindElement(By.XPath("//input[@id='opt_in']")).Displayed);
+            driver.FindElement(By.XPath("//input[@id='opt_in']")).SendKeys("y");
+            wait.Until(d => driver.FindElement(By.XPath("//div[@id='opt-in-container']/div/button")).Enabled);
+            driver.FindElement(By.XPath("//div[@id='opt-in-container']/div/button")).Click();
+            driver.Quit();
+        }
+    }
+}
